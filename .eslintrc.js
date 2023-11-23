ea28f730d672dfef9c9381dbbbc91795570bc627
HoshimuraYuto <sukiburo@gmail.com>
2023-11-23 11:55:11 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "import"],
+  root: true,
+  rules: {
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,25 @@
+# enhanced-typescript-node
+
+A Dev Container configuration for quickly setting up a TypeScript development environment.
+
+## Prerequisites
+
+To get started, launch the Dev Container and run the following command:
+
+```bash
+pnpm i
+```
+
+## Running TypeScript Files
+
+If you want to run TypeScript files without prioritizing execution speed, it is recommended to use [tsx](https://www.npmjs.com/package/tsx). tsx command can run both CommonJS and ESModule, so this is the recommended method for this setup.
+
+To run a TypeScript file, use the following command:
+
+```bash
+pnpm tsx index.ts
+```
+
+## License
+
+MIT-0 license
